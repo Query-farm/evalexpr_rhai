d92f5e9ba7c6a4371136006e225d7f3e05c81bd1
Rusty Conover <rusty@conover.me>
2025-10-14 23:12:32 -0400

    fix: fix for wasm builds

--- extension_config.cmake
+++ extension_config.cmake
@@ -4,6 +4,7 @@
 duckdb_extension_load(evalexpr_rhai
     SOURCE_DIR ${CMAKE_CURRENT_LIST_DIR}
     LOAD_TESTS
+    LINKED_LIBS "../../cargo/build/wasm32-unknown-emscripten/release/libduckdb_evalexpr_rhai_rust.a"
 )
 
 # Any extra extensions that should be built
